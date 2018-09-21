a6e37ae455a666a6edb0f51530ea298fcbf9e2dd
Hans Nieser <hans@reachdigital.nl>
2018-09-21 10:31:00 +0200

    Trigger source item indexer WIP

--- ISReservations/Model/AppendReservations.php
+++ ISReservations/Model/AppendReservations.php
@@ -9,6 +9,8 @@ namespace ReachDigital\ISReservations\Model;
 
 use Magento\Framework\Exception\CouldNotSaveException;
 use Magento\Framework\Exception\InputException;
+use Magento\InventoryIndexer\Indexer\SourceItem\SourceItemIndexer;
+use ReachDigital\ISReservations\Model\ResourceModel\GetSourceItemIdsFromReservations;
 use ReachDigital\ISReservations\Model\ResourceModel\SaveMultiple;
 use ReachDigital\ISReservationsApi\Model\ReservationInterface;
 use ReachDigital\ISReservationsApi\Model\AppendReservationsInterface;
@@ -30,19 +32,34 @@ class AppendReservations implements AppendReservationsInterface
     private $logger;
 
     /**
+     * @var SourceItemIndexer
+     */
+    private $sourceItemIndexer;
+
+    /**
+     * @var GetSourceItemIdsFromReservations
+     */
+    private $getSourceItemIdsFromReservations;
+
+    /**
      * @param SaveMultiple $saveMultiple
      * @param LoggerInterface $logger
+     * @param SourceItemIndexer $sourceItemIndexer
+     * @param GetSourceItemIdsFromReservations $getSourceItemIdsFromReservations
      */
     public function __construct(
         SaveMultiple $saveMultiple,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        SourceItemIndexer $sourceItemIndexer,
+        GetSourceItemIdsFromReservations $getSourceItemIdsFromReservations
     ) {
         $this->saveMultiple = $saveMultiple;
         $this->logger = $logger;
+        $this->sourceItemIndexer = $sourceItemIndexer;
+        $this->getSourceItemIdsFromReservations = $getSourceItemIdsFromReservations;
     }
 
     /**
-     * @todo Run indexer when a reservation has been made.
      * @inheritdoc
      */
     public function execute(array $reservations)
@@ -64,6 +81,9 @@ class AppendReservations implements AppendReservationsInterface
         }
         try {
             $this->saveMultiple->execute($reservations);
+            // @todo should we check if indexer is async?
+//            $sourceItemIds = $this->getSourceItemIdsFromReservations->execute($reservations);
+//            $this->sourceItemIndexer->executeList($sourceItemIds);
         } catch (\Exception $e) {
             $this->logger->error($e->getMessage());
             throw new CouldNotSaveException(__('Could not append Reservation'), $e);
---
+++ ISReservations/Model/ResourceModel/GetSourceItemIdsFromReservations.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+namespace ReachDigital\ISReservations\Model\ResourceModel;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Inventory\Model\ResourceModel\Source as SourceResourceModel;
+use Magento\Inventory\Model\ResourceModel\StockSourceLink as StockSourceLinkResourceModel;
+use Magento\Inventory\Model\StockSourceLink;
+use Magento\InventoryApi\Api\Data\SourceInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterface;
+
+class GetSourceItemIdsFromReservations
+{
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    public function __construct(
+      ResourceConnection $resourceConnection
+    ) {
+        $this->resourceConnection = $resourceConnection;
+    }
+
+    /**
+     * @param array $reservations
+     *
+     * @return array
+     * @throws \DomainException
+     */
+    public function execute(array $reservations): array
+    {
+        // @todo
+//        $connection = $this->resourceConnection->getConnection();
+//        $sourceTable = $this->resourceConnection->getTableName(SourceResourceModel::TABLE_NAME_SOURCE);
+//        $sourceStockLinkTable = $this->resourceConnection->getTableName(
+//            StockSourceLinkResourceModel::TABLE_NAME_STOCK_SOURCE_LINK
+//        );
+//
+//        $select = $connection->select()
+//            ->from(['source' => $sourceTable], [SourceInterface::SOURCE_CODE])
+//            ->joinInner(
+//                ['stock_source_link' => $sourceStockLinkTable],
+//                'source.' . SourceItemInterface::SOURCE_CODE . ' = stock_source_link.' . StockSourceLink::SOURCE_CODE,
+//                []
+//            )
+//            ->where('stock_source_link.' . StockSourceLink::STOCK_ID . ' = ?', $stockId)
+//            ->where(SourceInterface::ENABLED . ' = ?', 1);
+//
+//        return $connection->fetchCol($select);
+        return $reservations;
+    }
+}
