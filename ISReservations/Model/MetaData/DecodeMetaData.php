d8a43dbf2038ca56796d0ed4bd53aff6329d4b3d
Paul Hachmang <paul@h-o.nl>
2018-09-28 10:11:07 +0200

    Added DecodeMetaData and EncodeMetaData classes.

---
+++ ISReservations/Model/MetaData/DecodeMetaData.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+namespace ReachDigital\ISReservations\Model\MetaData;
+
+class DecodeMetaData
+{
+    public function execute(string $metaData) : array
+    {
+        $lineItems = explode(',', $metaData);
+        $values = [];
+        foreach ($lineItems as $lineItem) {
+            [$key, $value] = explode(':', $lineItem);
+            $values[$key] = $value;
+        }
+        return $values;
+    }
+}
---
+++ ISReservations/Model/MetaData/EncodeMetaData.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+namespace ReachDigital\ISReservations\Model\MetaData;
+
+class EncodeMetaData
+{
+
+    /**
+     * @param array $data
+     *
+     * @throws \Exception
+     */
+    public function execute(array $data) {
+        throw new \Exception('not yet implemented');
+    }
+}
